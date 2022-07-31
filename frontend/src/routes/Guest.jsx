b76f7287937c3c7fdd8802f4b29d32599ec74908
Code-hyunwoo <chichyun92@naver.com>
2022-07-31 11:11:36 +0900

    220731 feat: Postit

--- frontend/src/routes/Guest.jsx
+++ frontend/src/routes/Guest.jsx
@@ -1,5 +1,6 @@
 import styles from "./Guest.module.css"
 import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
 
 function Guest (){
 
@@ -10,9 +11,37 @@ function Guest (){
             <div className={styles.title}>
                 방명록
             </div>
-            <div className={styles.container} >
-                <h1>guest</h1>
-                <Link to='/' id={styles.back}><span> 돌아가기 </span></Link>
+            <div className={styles.container}>
+            <Grid id={styles.post_container} container>
+                <Grid 
+                item xs={12} sm={6} lg={3}
+                id={styles.rgyPostIt}>
+                    <div> 안녕하세요. 포스트잇 테스트 입니다. 글씨 남기고 갑니다. </div>    
+                </Grid>
+                <Grid 
+                item xs={12} sm={6} lg={3}
+                id={styles.rgyPostIt}>
+                    <div> 안녕하세요. 포스트잇 테스트 입니다. 글씨 남기고 갑니다. </div>    
+                </Grid>
+                <Grid 
+                item xs={12} sm={6} lg={3}
+                id={styles.rgyPostIt}>
+                    <div> 안녕하세요. 포스트잇 테스트 입니다. 글씨 남기고 갑니다. </div>    
+                </Grid>        
+                <Grid 
+                item xs={12} sm={6} lg={3}
+                id={styles.rgyPostIt}>
+                    <div> 안녕하세요. 포스트잇 테스트 입니다. 글씨 남기고 갑니다. </div>    
+                </Grid>        
+                <Grid 
+                item xs={12} sm={6} lg={3}
+                id={styles.rgyPostIt}>
+                    <div> 안녕하세요. 포스트잇 테스트 입니다. 글씨 남기고 갑니다. </div>    
+                </Grid>        
+            </Grid>
+                <div>
+                    <Link to='/' id={styles.back}><span> 돌아가기 </span></Link>
+                </div>
             </div>
         </>
     );
