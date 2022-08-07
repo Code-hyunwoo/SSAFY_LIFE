718b1e6dcbe6f254dd4fa105026c84d48e21bd43
Code-hyunwoo <chichyun92@naver.com>
2022-08-07 23:34:29 +0900

    220807 feat: Motion UI

--- frontend/src/routes/Motion.jsx
+++ frontend/src/routes/Motion.jsx
@@ -1,5 +1,5 @@
 import styles from './Motion.module.css'
-import { useSpring, animated, a } from 'react-spring'
+import { useSpring, animated, a, useTransition } from 'react-spring'
 import { useState, useEffect} from "react";
 import useMeasure from 'react-use-measure'
 
@@ -31,16 +31,55 @@ function Motion (){
         })
       }, [api])
 
-     
+    //Picture
+    const slides = [
+        'https://www.wallpapertip.com/wmimgs/0-3126_cool-wallpapers-moving-colorful-cool-backgrounds.jpg',
+        'https://wc.wallpaperuse.com/wallp/98-981647_s.jpg',
+        'https://mblogthumb-phinf.pstatic.net/MjAxODAyMDNfNDIg/MDAxNTE3NjMwODA2NDgx.uHrmzMsM3fYV1-K8Lb0lHQiilR-eT2MbpN-KUwAyU7Ag.LpRc_T15kntzJq05rMyp45hrx4wZRPclyKqIpH_Tr_gg.JPEG.knicjin/wallpaper_32.jpg?type=w800',
+        'https://downloadwap.com/thumbs2/wallpapers/p2ls/new/33/TrNMwzGE.jpg',
+      ]
+
+    const [index, setIndex] = useState(0)
+    const transitions = useTransition(index, {
+    key: index,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: 3000 },
+    onRest: (_a, _b, item) => {
+      if (index === item) {
+        setIndex(state => (state + 1) % slides.length)
+      }
+    },
+    exitBeforeEnter: true,
+  })
       
 
     return (
         <div className={styles.container}> 
+            <div className={styles.titlebox}>Motion UI</div>
+            {/* Picture */}
+            <div className="flex fill center">
+                  {transitions((style, i) => (
+                      <animated.div
+                      className={styles.bg}
+                      style={{
+                          ...style,
+                          backgroundImage: `url(${slides[i]})`,
+                      }}
+                      />
+                  ))}
+            </div>
+
+
+            {/* Graph */}
             <div ref={ref} className={styles.main} onClick={() => toggle(!open)}>
                 <animated.div className={styles.fill} style={props} />
                 <animated.div className={styles.content}>{props.width.to(x => x.toFixed(0))}</animated.div>
             </div>
             
+
+            {/* Flip */}
             <div className={styles.flip_container} onClick={() => set(state => !state)}>
             <a.div
                 className={`${styles.c} ${styles.back}`}
@@ -56,12 +95,14 @@ function Motion (){
             />
             </div>
             
+
+            {/* Move */}
             <div className={styles.move_container}>
                  <animated.div
             style={{
                 width: 100,
                 height: 100,
-                backgroundColor: '#46e891',
+                backgroundColor: 'red',
                 borderRadius: 16,
                 ...moves,
             }}
