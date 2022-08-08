f0c60350577b713feac435654a9c0c8ef3ff572b
Code-hyunwoo <chichyun92@gmail.com>
2022-08-08 19:39:31 +0900

    220808 feat: Database

--- Backend/dbConfig.js
+++ Backend/dbConfig.js
@@ -6,16 +6,16 @@ const mysql = require('mysql');
 
 
 const connection = mysql.createConnection({
-    // host:conf.host,
-    // user:conf.user,
-    // password:conf.password,
-    // port:conf.port,
-    // database:conf.database
-    host:"localhost",
-    user:"root",
-    password:"11223344",
-    port:"3306",
-    database:"hyunwoo"
+    host:conf.host,
+    user:conf.user,
+    password:conf.password,
+    port:conf.port,
+    database:conf.database
+    // host:"localhost",
+    // user:"root",
+    // password:"11223344",
+    // port:"3306",
+    // database:"hyunwoo"
 })
 
 // host:conf.host,
