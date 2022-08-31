18e98c0aa02d5f34bc03436bfebef92f620daf2d
Code-hyunwoo <chichyun92@gmail.com>
2022-08-31 16:55:34 +0900

    220831 feat: Wordle

--- Backend/controllers/wordleCtrl.js
+++ Backend/controllers/wordleCtrl.js
@@ -8,17 +8,23 @@ const wordleCtrl = {
             res.send(rows);
         })
     },
-    // insertGuest : async(req,res) => {
-    //     // javascript 구조 분해 할당
-    //     const {person_id, person_name, person_text, guest_date, guest_hour} = req.body;
-    //     const sql = `INSERT INTO guest(person_name,person_text,guest_date,guest_hour) VALUES('${person_name}','${person_text}','${guest_date}','${guest_hour}')`
-    //     connection.query(
-    //         sql,(error,rows) => {
-    //             if(error) throw error;
-    //             res.send(rows);
-    //         }
-    //     )
-    // }
+    insertRoyal : async(req,res) => {
+        // javascript 구조 분해 할당
+        const {royal_name, royal_date, royal_time} = req.body;
+        const sql = `INSERT INTO royal(name, date, time) VALUES('${royal_name}','${royal_date}','${royal_time}')`
+        connection.query(
+            sql,(error,rows) => {
+                if(error) throw error;
+                res.send(rows);
+            }
+        )
+    },
+    getRoyal : async (req, res) => {
+        connection.query('SELECT * FROM royal', (error, rows)=> {
+            if(error) throw error;
+            res.send(rows);
+        })
+    },
 }
 
 module.exports = wordleCtrl
--- Backend/routes/wordleRouter.js
+++ Backend/routes/wordleRouter.js
@@ -4,8 +4,10 @@ const router = require("express").Router();
 
 router.route('/')
     .get(wordleCtrl.getAnswer)
-    // .post(guestCtrl.insertGuest)
+    .post(wordleCtrl.insertRoyal)
 
+router.route('/royal')
+    .get(wordleCtrl.getRoyal)
 // api/movie/makeMovie
 // router.route('/makemovie')
 
